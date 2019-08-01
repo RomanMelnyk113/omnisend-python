cc27ee48b5e07d2e89c5268cedfb376f436249a4
roman <roman.melnyk20071992@gmail.com>
2019-08-01 13:27:37 +0300

    update `create_contact` method according to Marshmallow schema

--- omnisend/client.py
+++ omnisend/client.py
@@ -1,10 +1,11 @@
 import json
-from datetime import datetime
 from http import HTTPStatus
+from typing import Tuple, Union
 
 import requests
+from marshmallow import ValidationError
 
-from omnisend import ClientException
+from omnisend.models import Contact
 
 DEFAULT_BASE_URL = "https://api.omnisend.com/"
 API_VERSION_3 = "v3"
@@ -14,6 +15,7 @@ class Omnisend:
     # public key
     api_key: str
     api_url: str
+    errors: dict
 
     def __init__(self, api_key, api_url=None):
         self.api_key = api_key
@@ -25,35 +27,40 @@ class Omnisend:
     def _generate_url(self, endpoint):
         return self.api_url + endpoint
 
-    def _send_request(self, endpoint: str, data: dict):
-        headers = self._prepare_headers()
-        r = requests.post(self._generate_url(endpoint), headers=headers, json=data)
+    def _clear_errors(self):
+        self.errors = {}
+
+    def _send_request(self, endpoint: str, data: dict = None, method="post") -> dict:
+        kwargs = {"headers": self._prepare_headers()}
+        func = getattr(requests, method)
+        if method == "post":
+            kwargs["json"] = data
+
+        r = func(self._generate_url(endpoint), **kwargs)
+        res = json.loads(r.text)
         if r.status_code != HTTPStatus.OK:
-            raise ClientException("Omnisend error: {}".format(r.text))
+            # raise ClientException("Omnisend error: {}".format(r.text))
+            self.errors = res
 
-        return json.loads(r.text)
+        return res
 
-    def create_contact(self, contact_info: dict):
+    def create_contact(self, data: dict) -> Tuple[Union[dict, None], Union[dict, None]]:
         """
         Create new contact from provided data
         More details can be found here
         https://api-docs.omnisend.com/v3/contacts/create-contacts
 
-        :type contact_info: dict - new contact data
+        :type data: dict - new contact data
         """
+
+        self._clear_errors()
         endpoint = "/contacts"
-        if "email" not in contact_info:
-            raise AttributeError("Email field is required")
+        contact_schema = Contact()
+        try:
+            result = contact_schema.dump(data)
+        except ValidationError as err:
+            self.errors = err.messages
+            return None, self.errors
 
-        if "status" not in contact_info:
-            contact_info["status"] = "subscribed"
-
-        if "statusDate" not in contact_info:
-            contact_info["statusDate"] = datetime.utcnow().strftime(
-                "%Y-%m-%dT%H:%M:%SZ"
-            )
-        result = self._send_request(endpoint, contact_info)
-        return result
-
-    def get_contact(self, contact_id: str):
-        pass
+        response = self._send_request(endpoint, data=result, method="post")
+        return (None, self.errors) if self.errors else (response, None)
